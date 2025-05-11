3cd9c83fce5720cc683907a1c2872259785bd80b
mili <982074664@proton.me>
2025-05-11 12:02:27 +0800

    update

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="probctrl",
-    version="0.1.0",
+    version="0.1.1",
     description="A lightweight Python decorator toolkit for probabilistic, delayed, and throttled execution.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/probctrl",
+    author="0.1.0",
+    author_email="982074664@qq.com",
+    url="https://github.com/Lovelymili/probctrl",
     license="MIT",
     packages=find_packages(),
     python_requires=">=3.6",
